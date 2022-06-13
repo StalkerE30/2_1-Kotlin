7d3a0ba8c620542597af216c046338cd94c5ca6c
StalkerE30 <mpower_e30@rambler.ru>
2022-06-13 20:32:01 +0200

    Append comment

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,7 @@
 fun main() {
-    val rate = 0.0075
-    val amount = 5000_00
-    val fixTax = 35_00
+    val rate = 0.0075 //процент комиссии за перевод
+    val amount = 5000_00 //сумма перевода
+    val fixTax = 35_00 //значение минимальной комиссии за перевод
     val result: Int = (if (amount * rate < fixTax) fixTax else amount * rate).toInt()
     println("Комиссия за перевод составит: $result копеек")
 }
